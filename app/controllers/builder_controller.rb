5afb9cea76de65858394ac630715703261dbab1b
Gilbert <gilbertbgarza@gmail.com>
2011-03-15 17:06:52 -0500

    Removed unnecessary code

--- app/controllers/builder_controller.rb
+++ app/controllers/builder_controller.rb
@@ -1,7 +1,5 @@
 class BuilderController < ApplicationController
   def index
-    db = CouchRest.database("http://admin_chipotle:123123@yomobi.couchone.com/chipotle")
-    @widgets_in_use = widgets_in_use(db)
   end
   
   def update_widget
@@ -12,12 +10,5 @@ class BuilderController < ApplicationController
     res = db.save_doc(params)
     puts "Response: #{res.inspect}"
     success params
-  end
-  
-  private
-  
-  def widgets_in_use(db)
-    res = db.view('widgets/by_name')
-    res['rows'].map {|r| r['key'][1] if r['key'][0].nil? }.compact
   end
 end
