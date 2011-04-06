f3348c04ec76cac436cafef1e7aa8c487d52bc21
Gilbert <gilbertbgarza@gmail.com>
2011-04-06 16:37:47 -0500

    Added google maps widget

---
+++ public/javascripts/widgets/mobile/gmap.js
@@ -0,0 +1,34 @@
+(function ($) {
+  var mapUrlBase = 'http://maps.google.com/maps/api/staticmap?'
+    , mapUrlOptions = '&size=300x300&sensor=false'
+  ;
+  
+  window.widgetClasses.gmap = Widget.extend({
+
+    getFullAddress: function () {
+      return [this.get('addr1'), this.get('addr2'),
+              this.get('city') + ', ' + this.get('state'),
+              this.get('zip'),   this.get('country')].join(' ');
+    },
+
+    getMapUrl: function (zoomLevel) {
+      var address = encodeURIComponent(this.getFullAddress())
+        , zoomLevel = zoomLevel || 11
+      ;
+      return mapUrlBase + '&center=' +
+             address + '&markers=' + address +
+             '&zoom=' + zoomLevel + mapUrlOptions;
+    },
+    
+    getShowData: function () {
+      var extraData = {
+        fullAddress: this.getFullAddress(),
+        mapUrl: this.getMapUrl(),
+        mapUrl2: this.getMapUrl(14)
+      };
+      return _.extend({},this.toJSON(),extraData);
+    }
+  });
+  
+})(jQuery);
+
