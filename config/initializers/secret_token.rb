44233d8f82c434a9c0204edf9f14fa1575a37413
Gilbert <gilbertbgarza@gmail.com>
2011-05-24 04:58:57 -0500

    Changed devise secret_token in hope of solving heroku problems

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Yomobi::Application.config.secret_token = '3c8131d79166122c14869b403eac43273415c0fbcafbbb10d59b0e7130b97cfd0f8f52c6d407a69adf9b33e4b057567b2b6d53e384c77f80db78b3e73bb5a309'
+Yomobi::Application.config.secret_token = '3c8131d79166122c14869b403eac43273415c0fbcafbbb10d59c0e7130b97cfd0f8f52c6d407a69adf9b33e4b057567b2b6d53e384c77f80db78b3e73bb5a309'
