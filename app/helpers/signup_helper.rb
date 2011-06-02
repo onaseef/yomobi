a9d3821d63dda7d6bcf4cbc4be10891be7f35ba5
Gilbert <gilbertbgarza@gmail.com>
2011-06-02 15:25:59 -0500

    Added signup step arrows

--- app/helpers/signup_helper.rb
+++ app/helpers/signup_helper.rb
@@ -1,2 +1,13 @@
 module SignupHelper
+
+  def active_arrow?(current_step,target_step)
+    case current_step - target_step
+    when 0
+      'active'
+    when 1
+      'just-active'
+    else
+      'inactive'
+    end
+  end
 end
