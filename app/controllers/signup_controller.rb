1d8fee94961952415f3ea75c74ef9eb5114c6a43
Gilbert <gilbertbgarza@gmail.com>
2011-06-07 17:47:43 -0500

    Fixed an issue with lowercase letters and db_names

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -26,7 +26,7 @@ class SignupController < ApplicationController
 
     @errors['title'] = true unless data['title'].match /^[a-z0-9 _$()+-]{2,16}$/i
 
-    if data['site_url'].match(/^[a-z][a-z0-9 _$()+-]{2,16}$/i).nil?
+    if data['site_url'].match(/^[a-z][a-z0-9 _$()+-]{2,16}$/).nil?
       @errors['site_url'] = 'illegal'
     elsif couchdb_exists? data['site_url']
       @errors['site_url'] = 'taken'
