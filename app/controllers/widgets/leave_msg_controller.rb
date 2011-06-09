19eca46468755e32c36dde799ebed65fed2e53aa
Gilbert <gilbertbgarza@gmail.com>
2011-06-09 16:07:32 -0500

    Fixed leave_msg widget emailing bug

--- app/controllers/widgets/leave_msg_controller.rb
+++ app/controllers/widgets/leave_msg_controller.rb
@@ -1,14 +1,21 @@
 class Widgets::LeaveMsgController < ApplicationController
   
   def submit
-    return error('bad message') if (params[:msg_content] || '').empty?
+    unless params[:feedback].present? && params[:email].present? && params[:subject].present?
+      return error('bad message')
+    end
+
+    company = Company.find_by_name params[:company]
+    return error('bad message') if company.nil?
 
     UserMailer.leave_msg({
       :to => company.leave_msg_email,
       :subject => 'You received customer feedback!',
       :from => 'feedback@yomobi.com',
-      :feedback => params[:msg_content]
+      :customer_subject => params[:subject],
+      :customer_feedback => params[:feedback],
+      :customer_email => params[:email]
     }).deliver
-    return success :msg => params[:msg_content]
+    return success :msg => params[:feedback]
   end
 end
