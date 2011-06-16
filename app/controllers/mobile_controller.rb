cfd93c87591f69d2bf00d4c8fff37315e2deb8c2
Gilbert <gilbertbgarza@gmail.com>
2011-06-16 00:36:18 -0500

    Case-insensitive company urls

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -1,6 +1,6 @@
 class MobileController < ApplicationController
   def index
-    @company = Company.where(:db_name => params[:company]).first
+    @company = Company.where(:db_name => params[:company].downcase).first
     # TODO: create "Company not found" page and redirect there instead
     redirect_to root_path if @company.nil?
   end
