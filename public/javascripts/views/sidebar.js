4a07ea8da23b6bd4afcfb459a077b3752633154b
Gilbert <gilbertbgarza@gmail.com>
2011-06-19 17:32:20 -0500

    Added extra padding for horizontal scrollbar

--- public/javascripts/views/sidebar.js
+++ public/javascripts/views/sidebar.js
@@ -82,7 +82,7 @@
       e.preventDefault();
       if (this.winRef && !this.winRef.closed) { this.winRef.close() };
 
-      var emulatorWidth = ($('#mobile-container').height() < 480) ? 320 : 320+util.scrollbarWidth();
+      var emulatorWidth = ($('#mobile-container').height() < 480) ? 320 : 320+util.scrollbarWidth()+5;
       this.winRef = window.open(e.target.href,'Your_Mobile_Website',
         'width=' + emulatorWidth + ',height=480,scrollbars=yes');
     },
