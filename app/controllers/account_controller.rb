275f0c49170ab8e69005193924d56b83696cd229
Gilbert <gilbertbgarza@gmail.com>
2011-12-01 19:30:34 -0600

    Added email notification when user changes emails. Closes #44

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -9,12 +9,16 @@ class AccountController < ApplicationController
   def update
     @user = current_user
     u_params = params[:user]
+    old_email = @user.email
 
     if u_params[:current_password].blank?
       flash[:alert] = "Please enter your current password to update your account."
     elsif @user.update_with_password(u_params)
       sign_in(@user, :bypass => true)
       flash[:notice] = "Account updated successfully."
+      if @user.email != old_email
+        UserMailer.email_changed_notice(@user, old_email).deliver
+      end
     else
       error = @user.errors.first
       flash[:alert] = "Error: #{error[0].to_s.humanize} #{error[1]}"
