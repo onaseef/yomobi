24e4e5acba3701114ce7c1ecb8236f89a4327e84
Omar <onaseef@yahoo.com>
2011-12-09 20:16:47 -0600

    Added huawei to mobile device list

--- app/helpers/mobile_helper.rb
+++ app/helpers/mobile_helper.rb
@@ -1,6 +1,6 @@
 module MobileHelper
 
   def mobile_devices
-    "alcatel|android|au-mic|audiovox|avantgo|blackberry|blazer|bolt|cldc-|danger|dream|epoc|ericsson|ericy|htc|ipad|iphone|ipod|j2me|lg-|lge|midp-|mobile|mot|netfront|nintendo|nitro|nokia|opera\\smini|palm|palmsource|panasonic|philips|playstation|pocketpc|portalmmm|rover|samsung|sanyo|series60|sharp|sie-|smartphone|sony|symbian|teleca|up.browser|up.link|vnd.wap.xhtml+xml|vodafone|wap1|wap2|webos|windows\\sce"
+    "alcatel|android|au-mic|audiovox|avantgo|blackberry|blazer|bolt|cldc-|danger|dream|huawei|epoc|ericsson|ericy|htc|ipad|iphone|ipod|j2me|lg-|lge|midp-|mobile|mot|netfront|nintendo|nitro|nokia|opera\\smini|palm|palmsource|panasonic|philips|playstation|pocketpc|portalmmm|rover|samsung|sanyo|series60|sharp|sie-|smartphone|sony|symbian|teleca|up.browser|up.link|vnd.wap.xhtml+xml|vodafone|wap1|wap2|webos|windows\\sce"
   end
 end
