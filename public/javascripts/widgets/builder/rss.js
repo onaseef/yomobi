048a4a687960651ed42fcd0ab51462fbd5ce580e
Omar <onaseef@yahoo.com>
2012-03-01 19:41:30 -0600

    Updated RSS feed default to 25 and max to 50

--- public/javascripts/widgets/builder/rss.js
+++ public/javascripts/widgets/builder/rss.js
@@ -6,7 +6,7 @@
   window.widgetEditors.rss = window.EditWidgetView.extend({
 
     onEditStart: function (widget) {
-      var postCount = this.widget.get('postCount') || 10;
+      var postCount = this.widget.get('postCount') || 25;
       this.$('select[name=postCount]').val(postCount);
     }
   });
