e6dce016622a0929804c6d12d73ae8b1a3734183
Omar <onaseef@yahoo.com>
2012-03-06 18:34:25 -0600

    Fixed tablet "Call Us" widget issue.  Added iPod to the Tablet list

--- app/helpers/mobile_helper.rb
+++ app/helpers/mobile_helper.rb
@@ -4,6 +4,6 @@ module MobileHelper
     "alcatel|android|au-mic|audiovox|avantgo|blackberry|blazer|bolt|cldc-|danger|dream|huawei|epoc|ericsson|ericy|htc|ipad|iphone|ipod|kindle|j2me|lg-|lge|midp-|mobile|mot|netfront|nintendo|nitro|nokia|opera\\smini|palm|palmsource|panasonic|philips|playstation|pocketpc|portalmmm|rover|samsung|sanyo|series60|sharp|sie-|smartphone|sony|symbian|teleca|up.browser|up.link|vnd.wap.xhtml+xml|vodafone|wap1|wap2|webos|windows\\sce"
   end
   def tablets
-  	"ipad|kindle"
+  	"ipad|ipod|kindle"
   end
 end
