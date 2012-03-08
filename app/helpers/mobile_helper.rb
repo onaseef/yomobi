b87cfd43186a14c6b68ac54bc8968a62f4eba407
Omar <onaseef@yahoo.com>
2012-03-07 23:50:26 -0600

    Updated test for tablet devices.

--- app/helpers/mobile_helper.rb
+++ app/helpers/mobile_helper.rb
@@ -3,7 +3,7 @@ module MobileHelper
   def mobile_devices
     "alcatel|android|au-mic|audiovox|avantgo|blackberry|blazer|bolt|cldc-|danger|dream|huawei|epoc|ericsson|ericy|htc|ipad|iphone|ipod|kindle|j2me|lg-|lge|midp-|mobile|mot|netfront|nintendo|nitro|nokia|opera\\smini|palm|palmsource|panasonic|philips|playstation|pocketpc|portalmmm|rover|samsung|sanyo|series60|sharp|sie-|smartphone|sony|symbian|teleca|up.browser|up.link|vnd.wap.xhtml+xml|vodafone|wap1|wap2|webos|windows\\sce"
   end
-  def tablets
+  def tablet_devices
   	"ipad|ipod|kindle"
   end
 end
