ffd93a3c4576d407e7b6e2a2443602b8fc50a074
Omar <onaseef@yahoo.com>
2012-03-10 19:44:39 -0600

    Added additional mobile and tablet user agents

--- app/helpers/mobile_helper.rb
+++ app/helpers/mobile_helper.rb
@@ -1,9 +1,9 @@
 module MobileHelper
 
   def mobile_devices
-    "alcatel|android|au-mic|audiovox|avantgo|blackberry|blazer|bolt|cldc-|danger|dream|huawei|epoc|ericsson|ericy|htc|ipad|iphone|ipod|kindle|j2me|lg-|lge|midp-|mobile|mot|netfront|nintendo|nitro|nokia|opera\\smini|palm|palmsource|panasonic|philips|playstation|pocketpc|portalmmm|rover|samsung|sanyo|series60|sharp|sie-|smartphone|sony|symbian|teleca|up.browser|up.link|vnd.wap.xhtml+xml|vodafone|wap1|wap2|webos|windows\\sce"
+    "alcatel|android|au-mic|audiovox|avantgo|blackberry|blazer|bolt|cldc-|danger|dream|droid|droidx|huawei|epoc|ericsson|ericy|htc|ipad|iphone|ipod|kindle|j2me|lg-|lge|midp-|mobile|mot|netfront|nintendo|nitro|nokia|opera\\smini|palm|palmsource|panasonic|philips|playstation|pocketpc|portalmmm|rover|samsung|sanyo|series60|sharp|sie-|smartphone|sony|streak|symbian|teleca|up.browser|up.link|vnd.wap.xhtml+xml|vodafone|wap1|wap2|webos|windows|xoom\\sce"
   end
   def tablet_devices
-  	"ipad|ipod|kindle"
+  	"ipad|ipod|kindle|xoom"
   end
 end
