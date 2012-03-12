63960f17abb88b05e6a42835422006a46a8bd57a
Omar <onaseef@yahoo.com>
2012-03-11 23:45:20 -0500

    Removed "mobi" from mobile_devices list

--- app/helpers/mobile_helper.rb
+++ app/helpers/mobile_helper.rb
@@ -1,7 +1,7 @@
 module MobileHelper
 
   def mobile_devices
-    "alcatel|android|au-mic|audiovox|avantgo|blackberry|blazer|bolt|cldc-|danger|dream|droid|droidx|huawei|epoc|ericsson|ericy|htc|ipad|iphone|ipod|kindle|j2me|lg-|lge|midp-|mobi|mobile|mot|netfront|nintendo|nitro|nokia|opera\\smini|opera\\smobi|palm|palmsource|panasonic|philips|playstation|pocketpc|portalmmm|rover|samsung|sanyo|series60|sharp|sie-|smartphone|sony|streak|symbian|teleca|up.browser|up.link|vnd.wap.xhtml+xml|vodafone|wap1|wap2|webos|windows\\sce|xoom"
+    "alcatel|android|au-mic|audiovox|avantgo|blackberry|blazer|bolt|cldc-|danger|dream|droid|droidx|huawei|epoc|ericsson|ericy|htc|ipad|iphone|ipod|kindle|j2me|lg-|lge|midp-|mobile|mot|netfront|nintendo|nitro|nokia|opera\\smini|opera\\smobi|palm|palmsource|panasonic|philips|playstation|pocketpc|portalmmm|rover|samsung|sanyo|series60|sharp|sie-|smartphone|sony|streak|symbian|teleca|up.browser|up.link|vnd.wap.xhtml+xml|vodafone|wap1|wap2|webos|windows\\sce|xoom"
   end
   def tablet_devices
   	"ipad|ipod|kindle|xoom"
