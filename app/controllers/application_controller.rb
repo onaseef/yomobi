4f553f92271691ad5af2dd68bcbb8e80b31b217c
Gilbert <gilbertbgarza@gmail.com>
2012-05-24 17:19:25 -0500

    Fixed bug in case request does not have language header.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -70,8 +70,8 @@ class ApplicationController < ActionController::Base
   end
 
   def set_locale
-    lang = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
-    lang = nil unless lang.match /^(es)$/
+    lang = (request.env['HTTP_ACCEPT_LANGUAGE'] || '').scan(/^[a-z]{2}/).first
+    lang = nil unless lang && lang.match(/^(es)$/)
 
     if params[:locale].present? && params[:locale].match(/^(es|en)/)
       cookies[:locale] = params[:locale]
