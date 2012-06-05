f9e988b1de2899448fd11bb64e60b889e39e437e
Gilbert <gilbertbgarza@gmail.com>
2012-06-05 15:38:36 -0500

    Fixed a bug with widget validation. Closes #304

--- public/javascripts/widgets/builder/base.js
+++ public/javascripts/widgets/builder/base.js
@@ -62,7 +62,7 @@
     validate: function (attrs) {
       for (var key in attrs) {
         // check for strings
-        if (attrs[key].substring) attrs[key] = $.trim(attrs[key]);
+        if (attrs[key] && attrs[key].substring) attrs[key] = $.trim(attrs[key]);
       }
       this.beforeSave && this.beforeSave(attrs);
     }
