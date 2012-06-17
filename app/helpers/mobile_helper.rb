42ce7435089f6e2501de7c2d557060c46472faac
Omar <onaseef@yahoo.com>
2012-06-17 18:00:33 -0500

    Updated test for mobile redirect to prevent redirect when on a tablet

--- app/helpers/mobile_helper.rb
+++ app/helpers/mobile_helper.rb
@@ -4,6 +4,9 @@ module MobileHelper
     "alcatel|android|au-mic|audiovox|avantgo|blackberry|blazer|bolt|cldc-|danger|dream|droid|droidx|huawei|epoc|ericsson|ericy|htc|iphone|ipod|j2me|lg-|lge|midp-|mobile|mot|netfront|nintendo|nitro|nokia|opera\\smini|opera\\smobi|palm|palmsource|panasonic|philips|playstation|pocketpc|portalmmm|rover|samsung|sanyo|series60|sharp|sie-|smartphone|sony|streak|symbian|teleca|up.browser|up.link|vnd.wap.xhtml+xml|vodafone|wap1|wap2|webos|windows\\sce"
   end
   def tablet_devices
+  	"ipad|kindle|xoom"
+  end
+  def nonphone_devices
   	"ipad|ipod|kindle|xoom"
   end
 end
