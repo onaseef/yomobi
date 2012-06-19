7919464d6232bd11ebca3e541f4199dfb4cf73c0
Omar <onaseef@yahoo.com>
2012-06-18 20:08:24 -0500

    Added pan tech and acer501 to the list of tablet and non-phone devices

--- app/helpers/mobile_helper.rb
+++ app/helpers/mobile_helper.rb
@@ -4,9 +4,9 @@ module MobileHelper
     "alcatel|android|au-mic|audiovox|avantgo|blackberry|blazer|bolt|cldc-|danger|dream|droid|droidx|huawei|epoc|ericsson|ericy|htc|iphone|ipod|j2me|lg-|lge|midp-|mobile|mot|netfront|nintendo|nitro|nokia|opera\\smini|opera\\smobi|palm|palmsource|panasonic|philips|playstation|pocketpc|portalmmm|rover|samsung|sanyo|series60|sharp|sie-|smartphone|sony|streak|symbian|teleca|up.browser|up.link|vnd.wap.xhtml+xml|vodafone|wap1|wap2|webos|windows\\sce"
   end
   def tablet_devices
-  	"ipad|kindle|xoom"
+  	"a501|ipad|kindle|pantech|xoom"
   end
   def nonphone_devices
-  	"ipad|ipod|kindle|xoom"
+  	"a501|ipad|ipod|kindle|pantech|xoom"
   end
 end
