e945b1b4eed6feb52685e1a8b2445229852665e5
Gilbert <gilbertbgarza@gmail.com>
2012-07-03 14:34:29 -0500

    Set test environment for heroku.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,37 +1,64 @@
 Yomobi::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.app_domains = ['deelmob.com']
+  config.re_app_domains = config.app_domains.map {|d| Regexp.escape d}.join '|'
   config.heroku_app_name = 'yomobi-test'
 
-  # The test environment is used exclusively to run your application's
-  # test suite.  You never need to work with it otherwise.  Remember that
-  # your test database is "scratch space" for the test suite and is wiped
-  # and recreated between test runs.  Don't rely on the data there!
-  config.cache_classes = true
+  config.opt_out_url_host = ENV['DEVISE_URL_HOST'] || 'http://deelmob.com'
+
+  # In the development environment your application's code is reloaded on
+  # every request.  This slows down response time but is perfect for development
+  # since you don't have to restart the webserver when you make code changes.
+  config.cache_classes = false
 
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
+  config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = false
 
-  # Raise exceptions instead of rendering exception templates
-  config.action_dispatch.show_exceptions = false
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = false
 
-  # Disable request forgery protection in test environment
-  config.action_controller.allow_forgery_protection    = false
+  # Print deprecation notices to the Rails logger
+  config.active_support.deprecation = :log
 
-  # Tell Action Mailer not to deliver emails to the real world.
-  # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
+  # Only use best-standards-support built into browsers
+  config.action_dispatch.best_standards_support = :builtin
 
-  # Use SQL instead of Active Record's schema dumper when creating the test database.
-  # This is necessary if your schema can't be completely dumped by the schema dumper,
-  # like if you have constraints or database-specific column types
-  # config.active_record.schema_format = :sql
+  # use postfix on da local machine
+  # ActionMailer::Base.smtp_settings = {
+    # :host => "localhost"
+  # }
 
-  # Print deprecation notices to the stderr
-  config.active_support.deprecation = :stderr
+  # use gmail for test emailing
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default_url_options = { :host => "local.host:3000" }
+  # ActionMailer::Base.smtp_settings = {
+  #   :address  => "smtp.gmail.com",
+  #   :port  => 587,
+  #   :user_name  => "yomobi.test",
+  #   :password  => "y0Yoy@filler",
+  #   :authentication  => :plain,
+  #   :enable_starttls_auto => true
+  # }
+
+  if ENV['SES_KEY'] && ENV['SES_SECRET']
+    puts "Using separate auth keys for Amazon SES"
+    ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
+      :access_key_id => ENV['SES_KEY'],
+      :secret_access_key => ENV['SES_SECRET']
+  end
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = {
+    :host => ENV['ACTION_MAILER_HOST'] || 'local.host:3000'
+  }
+
+
+  Slim::Engine.set_default_options :pretty => true
 end
