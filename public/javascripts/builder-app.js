2ab0f04ac64a507b32a62574232f499d1da489b6
Gilbert <gilbertbgarza@gmail.com>
2012-07-09 14:13:45 -0500

    Fixed another edge case with no-widget sites. Closes #382

--- public/javascripts/builder-app.js
+++ public/javascripts/builder-app.js
@@ -401,6 +401,7 @@
       util.pushUIBlock('meta');
 
       $.post('/order',mapp.metaDoc,function (newMetaDoc) {
+        newMetaDoc.worder || (newMetaDoc.worder = {});
         mapp.metaDoc = newMetaDoc;
         util.clearUIBlock('meta');
         callback && callback();
