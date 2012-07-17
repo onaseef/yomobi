869feeeb0b83b0cfce7d13d7b560b4969c3010a9
Gilbert <gilbertbgarza@gmail.com>
2012-07-17 00:28:50 -0500

    Include preapproval_id in payment history. Closes #408

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -1,10 +1,11 @@
 class Charge
-  attr_reader :charge_date, :amount_paid, :site_url, :site_name
+  attr_reader :charge_date, :amount_paid, :site_url, :site_name, :preapproval_id
 
   def initialize(payment, charge_date)
     @charge_date = charge_date
     @amount_paid = payment.amount_paid
     @site_url = payment.company.site_url
     @site_name = payment.company.site_name
+    @preapproval_id = payment.wcr.preapproval_id
   end
 end
