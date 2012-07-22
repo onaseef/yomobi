19f05f1b58832176368231e043fdf4d9430b6646
Gilbert <gilbertbgarza@gmail.com>
2012-07-22 15:35:43 -0500

    Language header debug message.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -72,6 +72,7 @@ class ApplicationController < ActionController::Base
   def set_locale
     # Do the language check here since we support limited languages and
     # checking here is faster than a file system check.
+    puts "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
     lang = request.preferred_language_from I18n.available_locales
     lang ||= request.compatible_language_from I18n.available_locales
 
@@ -79,6 +80,7 @@ class ApplicationController < ActionController::Base
       cookies[:locale] = params[:locale]
     end
 
+    puts "** Using Cookie" if cookies[:locale]
     I18n.locale = cookies[:locale] || lang || I18n.default_locale
   end
 
