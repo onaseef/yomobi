5c1f8de39f43a681ab7b515173bda4ad9c88a5cf
Zac Moody <xanhast@gmail.com>
2012-08-21 16:18:59 +0100

    The return of % widths!

--- public/javascripts/views/customize.js
+++ public/javascripts/views/customize.js
@@ -232,6 +232,7 @@
       hasChanges || (hasChanges = true);
       var iconLayout = this.$('input[name=icon_layout]:checked').val();
       $('#home-widgets .home-icon .inner').toggleClass( 'line', iconLayout == 'line');
+      $('#home-widgets .home-icon').css('width', (iconLayout == 'line' ? '100%' : '25%'));
       resizeEmulator();
     },
 
