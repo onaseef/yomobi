b94f3321d061208141297347633f75125a196669
Omar <onaseef@yahoo.com>
2012-12-01 17:37:36 -0600

    Added PanTech to list of mobile_devices

--- app/helpers/mobile_helper.rb
+++ app/helpers/mobile_helper.rb
@@ -1,7 +1,7 @@
 module MobileHelper
 
   def mobile_devices
-    "alcatel|android|au-mic|audiovox|avantgo|blackberry|blazer|bolt|cldc-|danger|dream|droid|droidx|huawei|epoc|ericsson|ericy|htc|iphone|ipod|j2me|lg-|lge|midp-|mobile|mot|netfront|nintendo|nitro|nokia|opera\\smini|opera\\smobi|palm|palmsource|panasonic|philips|playstation|pocketpc|portalmmm|rover|samsung|sanyo|series60|sharp|sie-|smartphone|sony|streak|symbian|teleca|up.browser|up.link|vnd.wap.xhtml+xml|vodafone|wap1|wap2|webos|windows\\sce"
+    "alcatel|android|au-mic|audiovox|avantgo|blackberry|blazer|bolt|cldc-|danger|dream|droid|droidx|huawei|epoc|ericsson|ericy|htc|iphone|ipod|j2me|lg-|lge|midp-|mobile|mot|netfront|nintendo|nitro|nokia|opera\\smini|opera\\smobi|palm|palmsource|panasonic|pantech|philips|playstation|pocketpc|portalmmm|rover|samsung|sanyo|series60|sharp|sie-|smartphone|sony|streak|symbian|teleca|up.browser|up.link|vnd.wap.xhtml+xml|vodafone|wap1|wap2|webos|windows\\sce"
   end
   def tablet_devices
   	"a501|ipad|kindle|pantech|xoom"
