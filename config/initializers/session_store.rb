f32c18d59a4937483b4ca059e0d31f86398daab1
deflexor <deflexor@yandex.ru>
2013-02-15 21:06:40 +0400

    [#44352391] Session storage changed to mongoid

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,16 @@
 # Be sure to restart your server when you modify this file.
 
-Yomobi::Application.config.session_store :cookie_store, :key => '_yomobi_session'
+Yomobi::Application.config.session_store :mongoid_store
+module ActionDispatch
+  module Session
+    class MongoidStore < AbstractStore
+      def destroy_session(env, sid, options)
+        self.send(:destroy, env)
+        generate_sid unless options[:drop]
+      end
+    end
+  end
+end
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
