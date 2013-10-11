fa1317f5b01915f7607e2d54cbe43c0747aaf309
Omar <onaseef@yahoo.com>
2013-10-11 09:06:26 -0500

    Updated def tablet_devices and nonphone_devices to fix iOS 7.0.2 bug
    The iOS 7.0.2 user agent contains the string a501, which is also part
    of the ACER Tablet user agent, so iOS 7.0.2 devices stopped
    redirecting. Updated the ACER Tablet string from "a501" to "a501 build"

--- app/helpers/mobile_helper.rb
+++ app/helpers/mobile_helper.rb
@@ -4,9 +4,9 @@ module MobileHelper
     "alcatel|android|au-mic|audiovox|avantgo|blackberry|blazer|bolt|cldc-|danger|dream|droid|droidx|huawei|epoc|ericsson|ericy|htc|iphone|ipod|j2me|lg-|lge|midp-|mobile|mot|netfront|nintendo|nitro|nokia|opera\\smini|opera\\smobi|palm|palmsource|panasonic|pantech|philips|playstation|pocketpc|portalmmm|rover|samsung|sanyo|series60|sharp|sie-|smartphone|sony|streak|symbian|teleca|up.browser|up.link|vnd.wap.xhtml+xml|vodafone|wap1|wap2|webos|windows\\sce"
   end
   def tablet_devices
-  	"a501|ipad|kindle|pantech|xoom"
+  	"a501\\sbuild|ipad|kindle|pantech|xoom"
   end
   def nonphone_devices
-  	"a501|ipad|ipod|kindle|pantech|xoom"
+  	"a501\\sbuild|ipad|ipod|kindle|pantech|xoom"
   end
 end
